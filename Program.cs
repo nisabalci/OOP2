f20a273fb85fdac107065a4c435e02a3a641d65b
nisabalci <nisabalci00@gmail.com>
2021-01-29 02:52:41 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TuzelMusteri musteri1 = new TuzelMusteri();
+            musteri1.Id = 2;
+            musteri1.MusteriNo = "54321";
+            musteri1.SirketAdi = "NB Software";
+            musteri1.VergiNo = "1234567890";
+
+            //Gerçek Müşteri - Tüzel Müşteri
+            //SOLID
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Add(musteri1);
+            musteriManager.Add(musteri3);
+
+        }
+    }
+}
